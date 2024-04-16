a1487fe3f3275fdfc4b6e72be03fa80d10460a45
jaatdev <48481927+jaatdev@users.noreply.github.com>
2024-04-16 16:20:16 +0530

    Update Tooltip.js
    moving Tooltip from class component to functional component

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,52 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "../tooltip.css";
 
-export default class Tooltip extends Component {
-  // constructor to create state for this class
-  constructor() {
-    super();
-    this.state = {
-      direction: "my-tooltiptext t-top",
-      content: "Hover Content",
-    };
-  }
+const Tooltip = (props) => {
+  const [tooltipState, setTooltipState] = useState({
+    direction: "my-tooltiptext t-top",
+    content: "Hover Content",
+  });
 
-  //? function to change values if data change
-  hanldeTooltip = () => {
-    //?  setting content of the tooltip
-    this.setState({
-      content: this.props.content,
-    });
+  const handleTooltip = () => {
+    setTooltipState((prevState) => ({
+      ...prevState,
+      content: props.content,
+      direction: determineDirection(props.direction),
+    }));
+  };
 
-    //? Direction setting based on user input
-    if (this.props.direction === "top") {
-      this.setState({
-        direction: "my-tooltiptext t-top",
-      });
-    } else if (this.props.direction === "right") {
-      this.setState({
-        direction: "my-tooltiptext t-right",
-      });
-    } else if (this.props.direction === "left") {
-      this.setState({
-        direction: "my-tooltiptext t-left",
-      });
-    } else {
-      this.setState({
-        direction: "my-tooltiptext t-bottom",
-      });
+  const determineDirection = (direction) => {
+    switch (direction) {
+      case "top":
+        return "my-tooltiptext t-top";
+      case "right":
+        return "my-tooltiptext t-right";
+      case "left":
+        return "my-tooltiptext t-left";
+      default:
+        return "my-tooltiptext t-bottom";
     }
   };
 
-  render() {
-    return (
-      //? Binding component inside a tooltip
-      <div className="my-tooltip" onMouseEnter={this.hanldeTooltip}>
-        {/* span for tooltip text */}
-        <span className={this.state.direction}>{this.state.content}</span>
-        {/* component fetching data  from where it is called */}
-        {this.props.children}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="my-tooltip" onMouseEnter={handleTooltip}>
+      <span className={tooltipState.direction}>{tooltipState.content}</span>
+      {props.children}
+    </div>
+  );
+};
+
+export default Tooltip;
