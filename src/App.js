8e3ec3bd435c6588477b3d050988a636cf768b50
jaatdev <48481927+jaatdev@users.noreply.github.com>
2024-04-16 16:18:23 +0530

    Update App.js
    moving from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,81 +1,61 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import Tooltip from "./components/Tooltip";
 
-export default class App extends Component {
+const App = () => {
+  const [tooltipState, setTooltipState] = useState({
+    direction: "top",
+    content: "I'm a tooltip",
+  });
 
-  // constructor to create state for this class
-  constructor() {
-    super();
-    this.state = {
-      direction: "top",
-      content: "I'm a tooltip"
-    };
-  }
-
-  //? function to handle input content for hover by user itself
-  handleContentInput = (e) => {
-    console.log(e.target.value);
-    if (e.target.value.length > 0) {
-      this.setState({
-        content: e.target.value,
-      });
-    } else {
-      this.setState({
-        content: "Tooltip Content",
-      });
-    }
+  const handleContentInput = (e) => {
+    const content =
+      e.target.value.length > 0 ? e.target.value : "Tooltip Content";
+    setTooltipState((prevState) => ({
+      ...prevState,
+      content,
+    }));
   };
 
-  //? function to handle direction according to selected option wheather it is top, right, lefet, bottom
-  handleDirectionInput = (e) => {
-    console.log(e.target.value);
-    this.setState({
-      direction: e.target.value,
-    });
+  const handleDirectionInput = (e) => {
+    const direction = e.target.value;
+    setTooltipState((prevState) => ({
+      ...prevState,
+      direction,
+    }));
   };
-  render() {
-    return (
-      <div className="App">
+
+  return (
+    <div className="App">
       <div className="main">
-            {/* Start :For Dynamic tooltip content and direction of tooltip*/}
-            <div>
-            {/* heading */}
-            <h1>Tooltip</h1>
-            <div className="form-div">
-              <label className="form-label">Tooltip Content</label>
-              <input
-                type="text"
-                className="form-control"
-                onKeyUp={this.handleContentInput}
-              />
-            </div>
-            <div className="form-div">
-              <label>Direction</label>
-              <select
-                className="form-select"
-                onChange={this.handleDirectionInput}
-              >
-                <option value="top">Top</option>
-                <option value="left">Left</option>
-                <option value="right">Right</option>
-                <option value="bottom">Bottom</option>
-              </select>
-            </div>
-          </div>
-          {/* End :For Dynamic tooltip content and direction of tooltip */}
-  
-          {/* //? Tooltip has created successfully */}
+        <h1>Tooltip</h1>
+        <div className="form-div">
+          <label className="form-label">Tooltip Content</label>
+          <input
+            type="text"
+            className="form-control"
+            onKeyUp={handleContentInput}
+          />
+        </div>
+        <div className="form-div">
+          <label>Direction</label>
+          <select className="form-select" onChange={handleDirectionInput}>
+            <option value="top">Top</option>
+            <option value="left">Left</option>
+            <option value="right">Right</option>
+            <option value="bottom">Bottom</option>
+          </select>
+        </div>
+        <Tooltip
+          content={tooltipState.content}
+          direction={tooltipState.direction}
+        >
+          <Button />
+        </Tooltip>
+      </div>
+    </div>
+  );
+};
 
-          {/* //? Binding Content inside tooltip content */}
-          <Tooltip content={this.state.content} direction={this.state.direction}>
-            {/*//? Button component called */}
-            <Button />
-          </Tooltip>
-      </div>
-  
-      </div>
-    );
-  }
-}
+export default App;
